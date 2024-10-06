2054fd17c7b0ba5f5194fb8d5039c44be533b4f7
SeoYeomm <lsy629@ewhain.net>
2024-10-06 23:48:16 +0900

    fix: univ_category 글자 수 수정

--- userInfo/models.py
+++ userInfo/models.py
@@ -16,7 +16,7 @@ class Profile(models.Model):
     ]
 
     username = models.OneToOneField(User, on_delete=models.CASCADE, db_column='username', to_field='username')
-    univ_category = models.CharField(max_length=50, choices=[('4년제(5~6년제포함)', '4년제(5~6년제포함)'), ('전문대(2~3년제)', '전문대(2~3년제)'), ('해외대학', '해외대학'), ('학점은행제 대학', '학점은행제 대학'), ('원격대학', '원격대학'), ('기술대학', '기술대학')], default='4년제(5~6년제포함)')
+    univ_category = models.CharField(max_length=255, choices=[('4년제(5~6년제포함)', '4년제(5~6년제포함)'), ('전문대(2~3년제)', '전문대(2~3년제)'), ('해외대학', '해외대학'), ('학점은행제 대학', '학점은행제 대학'), ('원격대학', '원격대학'), ('기술대학', '기술대학')], default='4년제(5~6년제포함)')
     gender = models.CharField(max_length=10, choices=[('남성', '남성'), ('여성', '여성'), ('선택안함', '선택안함')], default='선택안함')
     age = models.SmallIntegerField()
     university = models.CharField(max_length=100)
