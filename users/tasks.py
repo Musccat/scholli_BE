f8c8f9aca464a0b99c3fdff0f69eb5dc8bdcf63e
hayong39 <yia0524_hy@naver.com>
2024-11-11 20:26:27 +0900

    feat: 이메일 인증 API, 회원가입 API

--- users/tasks.py
+++ users/tasks.py
@@ -1,3 +1,4 @@
+from django_redis import get_redis_connection
 from django.core.mail import EmailMessage
 from scholli.celery import app
 from .utils import sendEmailHelper
@@ -5,6 +6,7 @@ from .utils import sendEmailHelper
 @app.task
 def send_email(email):
     code = sendEmailHelper.make_random_code()
+    client.set(email, code, ex=300)  #email 키에 code 값을 300초(5분)동안 저장
     message = code
     subject = "%s" % "[SCHOLLI] 이메일 인증 코드 안내"
     to = [email]
