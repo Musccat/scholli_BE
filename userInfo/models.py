348bdf583ae37c0b4da0025f9380b78d68e8ceff
hayong39 <yia0524_hy@naver.com>
2024-11-21 15:22:37 +0900

    fix: UserSubscription 관련 User 수정

--- userInfo/models.py
+++ userInfo/models.py
@@ -55,7 +55,7 @@ class RecommendResult(models.Model):
     def __str__(self):
         return f"{self.user.username}에게 추천된 {self.scholarship.name}"
     
-User = get_user_model()
+# User = get_user_model()
 
 class UserSubscription(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="subscription")
