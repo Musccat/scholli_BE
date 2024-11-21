4e1f6d04e934f303de1289d74e57666777e2ad78
SeoYeomm <lsy629@ewhain.net>
2024-11-21 13:55:52 +0900

    fix: 구독 데코레이터 오류 수정

--- payment/utils.py
+++ payment/utils.py
@@ -5,15 +5,18 @@ from django.contrib.auth.models import AnonymousUser
 from userInfo.models import UserSubscription
 
 def subscription_required(view_func):
-    def _wrapped_view(request, *args, **kwargs):
-        if not request.user.is_authenticated:
+    def _wrapped_view(view, *args, **kwargs):
+        request = args[0]
+        user = getattr(request, 'user', None)
+
+        if not user or not user.is_authenticated:
             return Response(
                 {"error": "이 기능을 사용하려면 로그인해야 합니다."},
                 status=status.HTTP_401_UNAUTHORIZED,
             )
         
         try:
-            subscription = UserSubscription.objects.get(user=request.user)
+            subscription = UserSubscription.objects.get(user=user)
             subscription.check_subscription_status()  # 만료 상태 확인
             if not subscription.is_active:
                 return Response(
@@ -26,8 +29,9 @@ def subscription_required(view_func):
                 status=status.HTTP_403_FORBIDDEN,
             )
 
-        return view_func(request, *args, **kwargs)
+        return view_func(view, *args, **kwargs)
 
     return _wrapped_view
+
 
      
