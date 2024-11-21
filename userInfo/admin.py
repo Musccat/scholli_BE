56836bfd5f0a5c4208e555c500686f969d6580aa
SeoYeomm <lsy629@ewhain.net>
2024-11-21 15:33:06 +0900

    fix: 구독 admin 삭제

--- userInfo/admin.py
+++ userInfo/admin.py
@@ -7,8 +7,3 @@ admin.site.register(Profile)
 # Wishlist 모델
 admin.site.register(Wishlist)
 
-# 구독 모델
-@admin.register(UserSubscription)
-class UserSubscriptionAdmin(admin.ModelAdmin):
-    list_display = ('user', 'is_active', 'expiration_date')
-    list_filter = ('is_active',)
