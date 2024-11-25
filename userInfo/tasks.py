fb774d2bd5e3b5cc0f93939d4c93b27ebd921a58
hayong39 <yia0524_hy@naver.com>
2024-11-25 17:25:16 +0900

    Auto stash before merge of "main" and "origin/main"

--- userInfo/tasks.py
+++ userInfo/tasks.py
@@ -5,14 +5,15 @@ from django.utils.timezone import now, timedelta
 from .models import Wishlist
 from scholarships.models import Scholarship
 from userInfo.models import UserSubscription
+from django.core.mail import EmailMultiAlternatives
+
 
 @shared_task
-def send_deadline_email():
-    #이틀 후 마감되는 장학금 조회
+def send_deadline_email(email_html, recipient):
     two_days_from_now = now() + timedelta(days=2)
     scholarships = Scholarship.objects.filter(recruitment_end=two_days_from_now)
 
-    for scholarship in scholarships :
+    for scholarship in scholarships:
         #해당 장학금을 위시리스트에 추가한 사용자 조회
         wishlists = Wishlist.objects.filter(scholarship=scholarship)
 
@@ -22,17 +23,27 @@ def send_deadline_email():
                 # 사용자 구독 상태 확인
                 subscription = UserSubscription.objects.get(user=user)
                 if subscription.is_active:  # 구독이 활성화된 경우
-                    send_mail(
+                    # 템플릿 렌더링
+                    email_context = {
+                        'name': scholarship.name,
+                        'documents': scholarship.required_documents_details,
+                    }
+
+                    email_html = render_to_string('email_template.html', email_context)
+
+                    email=EmailMultiAlternatives(
                         subject='[SCHOLLI] 장학금 신청 마감 안내',
-                        message=(
-                            f"{scholarship.name} 장학금의 신청 마감일이 {scholarship.recruitment_end}입니다.\n\n"
-                            f"제출서류:\n{scholarship.required_documents_details}\n\n"
-                            "서둘러 신청하세요!"
-                        ),
-                        from_email=settings.DEFAULT_FROM_EMAIL,  # 발신자 이메일 (settings.py에 설정 필요)
-                        recipient_list=[user.email],  # 수신자 이메일
-                        )
+                        from_email=settings.DEFAULT_FROM_EMAIL,
+                        to=[user.email],
+                    )
+                    email.attach_alternative(email_html, "text/html")  # HTML 버전 첨부
+                    email.send()  # 이메일 발송
+                    
             except UserSubscription.DoesNotExist: # 구독 정보가 없는 경우 무시
                 continue
             except Exception as e:
-                print(f"Error sending email to {user.email}: {e}")
+                # 발송 실패 시 로깅
+                print(f"Error sending email to {recipient}: {e}")
+
+            
+                
